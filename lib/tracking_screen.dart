34652a9962f391b50e0259f12429506b4cbd96cb
Mostafa <mostafa.saad.emara@gmail.com>
2023-01-01 20:35:29 +0200

    config google maps

--- lib/tracking_screen.dart
+++ lib/tracking_screen.dart
@@ -1,11 +1,31 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class TrackingScreen extends StatelessWidget {
   const TrackingScreen({super.key});
-
+  static const CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final Completer<GoogleMapController> controller =
+        Completer<GoogleMapController>();
+    return Scaffold(
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          //   controller.complete(controller);
+        },
+      ),
+    );
   }
 }
